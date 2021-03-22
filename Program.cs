5dad38b52d1adc63439f2427727b869ddc6030c4
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-21 21:12:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MilhasParaKM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string medida;
+            double milhas, km;
+
+            Console.Write("Coloque abaixo uma medida em milhas para ser convertida para KM: ");
+            medida = Console.ReadLine (); 
+
+            milhas = Convert.ToDouble (medida);
+
+            km = milhas * 1.609;
+
+            Console.WriteLine("----------"); 
+            Console.WriteLine($"A medida em km é: {km}");
+        }
+    }
+}
